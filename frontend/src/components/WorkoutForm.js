478a34582daa0d210cde9462173b4bfa997ff6c8
sawepeter <sawepeter6@gmail.com>
2023-03-28 01:53:55 +0300

    Workout Form

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -4,6 +4,23 @@ const WorkoutForm = () => {
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
+    const [error, setError] = useState(null)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const workout = {title, load, reps}
+
+        const response = await fetch('/api/workouts', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
+        const json = await response.json()
+        
+    }
 
     return (
         <form className="create">
