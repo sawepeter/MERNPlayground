0274e82f2f3fc7cb06350eee828993d6541738a5
sawepeter <sawepeter6@gmail.com>
2023-04-12 19:21:50 +0300

    CRUD MERN app done

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
             <div className="container">
                 <Link to="/">
                     <h1>Workout Buddy</h1>
+                    {/* <img src={require('../images/logo.svg').default} alt="Workout Buddy" width={100} height={100} /> */}
                 </Link>
 
             </div>
