8e870c09a51ff7111c0053e2edd590cffa69d5a0
sawepeter <sawepeter6@gmail.com>
2023-04-18 16:11:52 +0300

    registering routes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,8 +6,13 @@ const Navbar = () => {
             <div className="container">
                 <Link to="/">
                     <h1>Workout Buddy</h1>
-                    {/* <img src={require('../images/logo.svg').default} alt="Workout Buddy" width={100} height={100} /> */}
                 </Link>
+                <nav>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>
+                </nav>
 
             </div>
         </header>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -15,12 +15,13 @@ const Login = () => {
         <form className="login" onSubmit={handleSubmit}>
             <h3>Log In</h3>
 
-            <label>Email address:</label>
+            <label>Email address :</label>
             <input
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
             />
+            <label>Password :</label>
             <input
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
