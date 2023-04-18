4e4077deb0cf3c50ed5e38aa8fe25e91f7bff7c6
sawepeter <sawepeter6@gmail.com>
2023-04-19 01:26:04 +0300

    logout hook

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout';
 
 const Navbar = () => {
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+        logout()
+    }
     return (
         <header>
             <div className="container">
