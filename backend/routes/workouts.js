f6a2b9d76e5a1a14d80056aae4b8e2cae7e42d34
sawepeter <sawepeter6@gmail.com>
2023-04-19 18:39:33 +0300

    protecting routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -5,8 +5,12 @@ const {
     getWorkouts,
     deleteWorkout,
     updateWorkout } = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
+
+//require auth for all workout routes
+router.use(requireAuth)
 
 //get all workouts
 router.get('/', getWorkouts)
