fa800898d284dff6035917062483f4bac5514c38
sawepeter <sawepeter6@gmail.com>
2023-04-23 09:28:15 +0300

    testing authorized requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,11 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import { useAuthContext } from './hooks/useAuthContext';
+//import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
 
-  const { user } = useAuthContext()
+  //const { user } = useAuthContext()
 
   return (
     <div className="App">
@@ -19,16 +19,19 @@ function App() {
           <Routes>
             <Route 
               path="/"
-              element={user ? <Home /> : <Navigate to="/login"/>}
+              // element={user ? <Home /> : <Navigate to="/login"/>}
+              element={<Home />}
                />
             <Route
               path="/login"
-              element={!user ? <Login/> : <Navigate to="/" />}
+              // element={!user ? <Login/> : <Navigate to="/" />}
+              element={<Login />}
               />
 
             <Route
               path="/signup"
-              element={!user ? <Signup/> : <Navigate to="/" />}
+              // element={!user ? <Signup/> : <Navigate to="/" />}
+              element={<Signup />}
               />
 
           </Routes>
